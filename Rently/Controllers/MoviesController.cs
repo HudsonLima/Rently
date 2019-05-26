bd0725bee2c75cb994a4bae8256c3bc484524353
Hudson Lima <hudsonlima.ti@gmail.com>
2019-05-26 06:21:00 -0300

    Adding Genre to Movies

--- Rently/Controllers/MoviesController.cs
+++ Rently/Controllers/MoviesController.cs
@@ -1,5 +1,7 @@
 ﻿using Rently.Models;
 using Rently.ViewModel;
+using System.Data.Entity;
+using System.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,19 @@ namespace Rently.Controllers
 {
     public class MoviesController : Controller
     {
+
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Movies
         public ActionResult Random()
         {
@@ -38,7 +53,7 @@ namespace Rently.Controllers
         // movies
         public ViewResult Index()
         {
-            var movie = GetMovies();
+            var movie = _context.Movies.Include(c => c.Genre).ToList();
             return View(movie);
         }
 
@@ -47,14 +62,15 @@ namespace Rently.Controllers
         {
             return Content(year + "/" + month);
         }
-
-        private IEnumerable<Movie> GetMovies()
+        
+        public ActionResult Details(int id)
         {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
     }
 }
