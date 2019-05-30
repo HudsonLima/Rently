395522e853c0a94ee21ce7201fdb3b70afe111c4
Hudson Lima <hudsonlima.ti@gmail.com>
2019-05-29 21:53:33 -0300

    Adding new view model and form

---
+++ Rently/ViewModels/MovieFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Rently.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rently.ViewModels
+{
+    public class MovieFormViewModel
+    {
+        public IEnumerable<Genre> GenreTypes { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
