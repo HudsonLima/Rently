1912591e304c9e676bf2296855f101e1539a93f8
Hudson Lima <hudsonlima.ti@gmail.com>
2019-06-02 22:48:09 -0300

    Redefining model to better fits in validation

--- Rently/ViewModels/MovieFormViewModel.cs
+++ Rently/ViewModels/MovieFormViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Rently.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,40 @@ namespace Rently.ViewModels
     public class MovieFormViewModel
     {
         public IEnumerable<Genre> GenreTypes { get; set; }
-        public Movie Movie { get; set; }
+
+        public int? Id { get; set; }
+
+        public string Name { get; set; }
+              
+        [Required]
+        [Display(Name = "Genre")]
+        public byte? GenreId { get; set; }
+              
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Display(Name = "Number In Stock")]
+        [Range(1, 20, ErrorMessage = "The field Number in Stock must be between {1} and {2}.")]
+        [Required]
+        public byte? NumberInStock { get; set; }
+        public List<Genre> Genres { get; set; }
+
+        public MovieFormViewModel()
+        {
+            Id = 0;
+
+        }
+
+        public MovieFormViewModel(Movie movie)
+        {
+            Id = movie.Id;
+            Name = movie.Name;
+            ReleaseDate = movie.ReleaseDate;
+            NumberInStock = movie.NumberInStock;
+            GenreId = movie.GenreId;
+        }
     }
+
+
 }
